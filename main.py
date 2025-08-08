f8c33e1292e9434099df5121f76a90118471f6ba
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-08 10:01:33 +0430

    text & ans input code .

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ from googletrans import Translator
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 translator = Translator()
+
+text = ""
+ans = input("Do you want the photos to be translated into Persian?(y/n): ")
